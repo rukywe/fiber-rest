248decdbd8d4dea22ce943f92ac65ebdeccef614
Kelvin Anigboro <rukywe@me.com>
2023-07-26 16:46:12 +0100

    add delete user logic

--- routes/user.go
+++ routes/user.go
@@ -114,3 +114,25 @@ func UpdateUser(c *fiber.Ctx) error {
 	return c.Status(200).JSON(responseUser)
 
 }
+
+func DeleteUser(c *fiber.Ctx) error {
+
+	id , err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure ID is an Integer")
+	}
+
+	if err:= findUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := database.Database.Db.Delete(&user).Error; err != nil{
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.Status(200).SendString("Successfully deleted user")
+
+}
